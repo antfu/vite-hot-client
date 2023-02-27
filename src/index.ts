9b9f948dd14ba6a92633a41806daf77b061f72f8
Anthony Fu <anthonyfu117@hotmail.com>
2023-02-27 17:21:17 +0100

    feat: introduce `tryCreateHotContext`

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,39 @@ export interface ViteClient {
 /**
  * Get the module of `/@vite/client`
  */
-export async function getViteClient(base = '/'): Promise<ViteClient | undefined> {
+export async function getViteClient(base = '/', warning = true): Promise<ViteClient | undefined> {
   try {
     return await import(/* @vite-ignore */ `${base}@vite/client`)
   }
-  catch {}
+  catch {
+    if (warning)
+      console.error(`[vite-plugin-hot] Failed to import "${base}@vite/client"`)
+  }
   return undefined
 }
 
 export async function createHotContext(path = '/____', base = '/'): Promise<ViteHotContext | undefined> {
   const viteClient = await getViteClient(base)
   return viteClient?.createHotContext(path)
+}
+
+/**
+ * Guess the vite client provided bases from the current pathname.
+ */
+export function guessBasesFromPathname(pathname = window.location.pathname) {
+  return pathname.split('/').map((i, idx, arr) => arr.slice(0, idx + 1).join('/') || '/')
+}
+
+/**
+ * Try to resolve the vite client provided bases.
+ */
+export async function tryCreateHotContext(path = '/___', bases?: string[]): Promise<ViteHotContext | undefined> {
+  bases = bases ?? guessBasesFromPathname()
+  for (const base of bases) {
+    const viteClient = await getViteClient(base, false)
+    const hot = viteClient?.createHotContext(path)
+    if (hot)
+      return hot
+  }
+  console.error(`[vite-plugin-hot] Failed to import vite client, tried with:`, bases)
 }
